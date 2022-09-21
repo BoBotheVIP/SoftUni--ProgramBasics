2e7caed99f190682eccdbd64f5b0f9644685e03a
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-09-22 01:29:36 +0300

    Create  10. USD to BGN.cs

---
+++ First Steps In Coding C#/10. USD to BGN.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            Console.WriteLine(bgn);
+
+
+
+        }
+        
+    }
+}
+
+
+
+
+
+
+
