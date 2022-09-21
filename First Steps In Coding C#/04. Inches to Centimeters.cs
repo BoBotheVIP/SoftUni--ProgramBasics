3e8d9fae029315607a2a800108bf67c17b1b85a9
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-09-22 01:14:11 +0300

    Create 04. Inches to Centimeters.cs

---
+++ First Steps In Coding C#/04. Inches to Centimeters.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MyApp
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.WriteLine(centimeters);
+            
+        }
+
+    }
+
+}
+
+
+
