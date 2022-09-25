266383dfe4db263fc5b60c895547c78733e14cec
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-09-25 14:59:07 +0300

    Create 03. Even or Odd.cs

---
+++ if, if-else/03. Even or Odd.cs
@@ -0,0 +1,21 @@
+using System;
+ 
+namespace hpmeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int num = int.Parse(Console.ReadLine());
+          if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }  
+          else
+            {
+                Console.WriteLine("odd");
+            }
+
+        }
+    }
+}
