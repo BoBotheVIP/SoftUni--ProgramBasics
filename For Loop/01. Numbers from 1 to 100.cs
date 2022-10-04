91d85e50c409ae0d4310c61417ca8b99fe415cb1
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-10-04 15:26:26 +0300

    Create 01. Numbers from 1 to 100.cs

---
+++ For Loop/01. Numbers from 1 to 100.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Numbersfrom1to100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int number=1; number<=100; ++number)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
