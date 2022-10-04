adf9bdbfb95ba29e36769e9a2db4399e3a07f9a1
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-10-05 01:08:11 +0300

    Create 08. Number sequence.cs

---
+++ For Loop/08. Number sequence.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for(int i = 0; i < n; i++)
+            {
+                int currNumber = int.Parse(Console.ReadLine());
+
+                if (currNumber > maxNumber)
+                {
+                    maxNumber = currNumber;
+                }
+                if(currNumber < minNumber)
+                {
+                    minNumber = currNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
