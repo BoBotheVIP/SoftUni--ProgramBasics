682da7e3152fa265411534cfcfd740dff3308032
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-10-12 15:42:45 +0300

    Create 03. Sum Numbers.cs

---
+++ While Loop/03. Sum Numbers.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int targetSum = int.Parse(Console.ReadLine());
+           int sum = 0;
+            while (true)
+            {
+                if (sum >= targetSum) 
+                { 
+                    break; 
+                }
+                int currNum = int.Parse(Console.ReadLine());
+                sum += currNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
