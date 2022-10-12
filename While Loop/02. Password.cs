e3218eb0bb5c41f3b55b7b3872eac1befd1adc02
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-10-12 15:26:03 +0300

    Create 02. Password.cs

---
+++ While Loop/02. Password.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username=Console.ReadLine();
+            string password=Console.ReadLine();
+
+            while(Console.ReadLine() != password) { }
+       
+            Console.WriteLine($"Welcome {username}!");
+            
+        }
+    }
+}
