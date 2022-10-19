4d5e28c1343ee1687826c04b67c69174b0a90560
Bozhidar <114099134+BoBotheVIP@users.noreply.github.com>
2022-10-19 22:42:42 +0300

    Create 02. Exam Preparation.cs

---
+++ While Loop/02. Exam Preparation.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace ExamPreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int filedTimes = int.Parse(Console.ReadLine());
+           double evaluationSum = 0;
+            int evaluationCount = 0;
+            int filedCount = 0;
+            string lastExercise = string.Empty;
+            int evaluation;
+            string input = Console.ReadLine();
+
+            while (input!="Enough")
+            {
+                lastExercise = input;
+                evaluation=int.Parse(Console.ReadLine());
+                evaluationCount++;
+                evaluationSum+=evaluation;
+                if (evaluation <= 4)
+                {
+                    filedCount++;
+                    if (filedCount >= filedTimes)
+                    {
+                        Console.WriteLine($"You need a break, {filedCount} poor grades.");
+                        break;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            if (input == "Enough")
+            {
+                 Console.WriteLine($"Average score: {evaluationSum/evaluationCount:f2}");
+                 Console.WriteLine($"Number of problems: {evaluationCount}");
+                Console.WriteLine ($"Last problem: {lastExercise}");
+            }
+            
+        }
+    }
+}
